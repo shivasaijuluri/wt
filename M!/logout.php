115239cefd6f20f4ec3a84841e82eea92dc739e0
Mr. Shiva <shivasaijuluri2004@gmail.com>
2024-03-05 08:18:46 +0530

    updated

--- M!/logout.php
+++ M!/logout.php
@@ -1,6 +1,6 @@
 <?php
 session_start(); 
 session_destroy(); 
-header("Location: index.html"); // Redirect to login page
-exit; // Ensure script stops executing after redirect
+header("Location: index.html"); 
+exit; 
 ?>
